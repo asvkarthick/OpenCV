b6d327e3413576d5143883908ef0d2097b24417a
Karthick Kumaran <asvkarthick@gmail.com>
2018-12-30 19:57:08 -0800

    Added program to convert RGB image to grayscale

---
+++ Tutorials/Tutorial_05/tutorial05.cpp
@@ -0,0 +1,40 @@
+// This program reads color image and converts it to grayscale image
+// and displays both RGB and grayscale image
+
+// Compile and build this program with
+// g++ tutorial05.cpp -o tutorial05 `pkg-config --cflags --libs opencv`
+
+// Run this program with
+// ./tutorial05
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+
+using namespace cv;
+
+int main(void)
+{
+    cv::Mat image = imread("../../images/lenna.bmp", CV_LOAD_IMAGE_COLOR);
+    if(!image.data)
+    {
+        std::cerr << "Could not open image file" << std::endl;
+        return -1;
+    }
+
+    cv::Mat gray_image;
+    // Convert RGB image to grayscale image
+    cvtColor(image, gray_image, CV_BGR2GRAY);    
+
+    namedWindow("RGB Image", WINDOW_AUTOSIZE);
+    imshow("RGB Image", image);
+
+    namedWindow("Grayscale Image", WINDOW_AUTOSIZE);
+    imshow("Grayscale Image", gray_image);
+
+    waitKey(0);
+    destroyAllWindows();
+
+    return 0;
+}
