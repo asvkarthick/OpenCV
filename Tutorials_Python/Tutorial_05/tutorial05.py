e163d52ed60313951ae081f6beec14a3310d1fe2
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-22 01:38:06 -0700

    Program to capture video with web camera
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ Tutorials_Python/Tutorial_05/tutorial05.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Author : Karthick Kumaran <asvkarthick@gmail.com>
+# Video capture program
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (0, 0), fx = 0.5, fy = 0.5)
+    cv2.imshow("Frame", frame)
+
+    ch = cv2.waitKey(1)
+    if ch & 0xFF == ord('x') : break
+
+cap.release()
+cv2.destroyAllWindows()
