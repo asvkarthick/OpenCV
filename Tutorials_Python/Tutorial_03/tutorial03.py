6d51d6e0c4eef01ed3793bf11432c4b2000a1263
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-22 01:04:35 -0700

    Added code to write the grayscale converted image to disk
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

--- Tutorials_Python/Tutorial_03/tutorial03.py
+++ Tutorials_Python/Tutorial_03/tutorial03.py
@@ -9,5 +9,8 @@ gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 cv2.imshow("Original", img)
 cv2.imshow("Gray", gray)
 
+print('Writing the image to disk')
+cv2.imwrite('grayscale.jpg', gray)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
