7cf91ded82429bf5d0fc9bfcb85bdf92b21cc65f
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-22 00:55:56 -0700

    Added cv2.split() functionality
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

--- Tutorials_Python/Tutorial_02/tutorial02.py
+++ Tutorials_Python/Tutorial_02/tutorial02.py
@@ -7,5 +7,10 @@ import cv2
 img = cv2.imread("../../images/dollar.bmp")
 cv2.imshow("Image", img)
 
+b,g,r = cv2.split(img)
+cv2.imshow("Blue", b)
+cv2.imshow("Green", g)
+cv2.imshow("Red", r)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
