29f563aa17e32a0b11d6f09656d8123b50803f2c
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-22 00:37:26 -0700

    Added simple OpenCV python program
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ Tutorials_Python/Tutorial_01/tutorial01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Author : Karthick Kumaran <asvkarthick@gmail.com>
+
+import numpy as np
+import cv2
+
+black_img = np.zeros([1280, 720, 1], 'uint8')
+cv2.imshow("Black Image", black_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+white_img = np.zeros([720, 1280, 1], 'uint8')
+white_img[:, :] = 255
+cv2.imshow("White Image", white_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
